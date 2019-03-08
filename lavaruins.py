0c2c774c0cff548f36125fc1991626af7563e067
tubuliferous <tubuliferous@gmail.com>
2019-03-08 16:36:34 -0600

    Add ability to take TSV input file

--- lavaruins.py
+++ lavaruins.py
@@ -79,13 +79,16 @@ def parse_file_contents(contents, filename, date):
     content_type, content_string = contents.split(',')
     decoded = base64.b64decode(content_string)
     try:
-        if 'csv' in filename:
+        if '.csv' in filename:
             # Assume that the user uploaded a CSV file
             df = pd.read_csv(
                 io.StringIO(decoded.decode('utf-8')))
-        elif 'xls' in filename:
+        elif '.xls' in filename:
             # Assume that the user uploaded an excel file
             df = pd.read_excel(io.BytesIO(decoded))
+            # Assume that the user uploaded a TSV file 
+        elif '.tsv' in filename:
+            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')), sep='\t')
 
         # !!Hack: set adjusted p-values beyond numerical precision of Excel
         # smallest_float = find_float_limits()[0]
